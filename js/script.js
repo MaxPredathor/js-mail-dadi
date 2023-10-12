c90ffcb43ceeb33f3b41fae8889dae70e4ec4508
MaxPredathor <max.gilli98@gmail.com>
2023-10-12 13:54:51 +0200

    defined array

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const validEmail = [
+    "max.gilli98@gmail.com",
+    "vin.derimanda@icloud.com",
+    "manni.federico@libero.com",
+    "riccardo.gabba02@gmail.com",
+    "mirko.marasco#gmail.com"
+];
+const emailCheck = 
